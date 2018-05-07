46ca19b5c56399f884c4df1eae4da1abc44eb73d
Lucas Marques <lucasmarques73@hotmail.com>
2018-05-07 17:13:04 -0300

    ex2

--- aula01/exData/ex2.php
+++ aula01/exData/ex2.php
@@ -6,3 +6,22 @@
 // Senão, boleto está vencido
 // mostra a msg 'Boleto vencido em X dias'
 // Form ou readline
+
+$data = readline('data de vencimento: ');
+
+$dataVenc = DateTime::createFromFormat('d/m/Y', $data);
+$dataAtual = new DateTime(date('Y-m-d') . '00:00:00');
+
+if ($dataVenc->format('l') == 'Saturday') {
+	$dataVenc->modify('+2 days');
+}
+if ($dataVenc->format('l') == 'Sunday') {
+	$dataVenc->modify('+1 day');
+}
+
+if ($dataVenc > $dataAtual) {
+	echo 'Pode Pagar Boleto'. PHP_EOL;
+}else {
+	$diff = $dataVenc->diff($dataAtual);
+	echo 'Boleto vencido à ' . $diff->format('%r%a') . ' dias' . PHP_EOL;
+}
