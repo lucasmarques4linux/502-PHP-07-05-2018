61a5377b76dce27c7f967b5ddf475c81f61f5312
Lucas Marques <lucasmarques73@hotmail.com>
2018-05-08 19:27:24 -0300

    Update lock.php

--- aula02/lock.php
+++ aula02/lock.php
@@ -6,7 +6,7 @@ $arquivo = fopen('arquivo.txt', 'w+');
 // LOCK_EX - Lock exclusivo
 // LOCK_UN - Libera Lock
 
-if (flock($arquivo, LOCK_EX)) {
+if (flock($arquivo, LOCK_EX | LOCK_NB)) {
 	fwrite($arquivo, 'Olá');
 	sleep(10);
 	flock($arquivo, LOCK_UN);
