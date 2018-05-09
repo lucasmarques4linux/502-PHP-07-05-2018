1b9c1e0c20855e127bdf2b5d508bc6c307391977
Lucas Marques <lucasmarques73@hotmail.com>
2018-05-09 16:22:45 -0300

    soap

--- aula03/SOAP/client.php
+++ aula03/SOAP/client.php
@@ -4,4 +4,13 @@ ini_set('soap.wsdl_cache_enabled', 0);
 
 $soapClient = new SoapClient('users.wsdl');
 
-echo $soapClient->list();
+
+
+$user = ['name' => 'Pedro' , 'email' => 'pedro@gmail.com', 'pass' => '234'];
+
+echo $soapClient->insert($user);
+
+$users = json_decode($soapClient->list()) ;
+foreach ($users as $user) {
+	echo $user->name . '<br>';
+}
